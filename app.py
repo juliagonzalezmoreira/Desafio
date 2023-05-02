8643975f68def5d5dab92de94cfdcabccf2b8bad
Julia Gonzalez Moreira <juliagonzalezmoreira@gmail.com>
2023-05-02 10:27:23 -0300

    banco de dados
    conexão com o banco de dados (mysql)

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask, url_for, render_template
-""" from flask_mysqldb import MySQL """
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-""" app.config['MYSQL_Host'] = 'localhost'
+app.config['MYSQL_Host'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'ju070205'
-app.config['MYSQL_DB'] = 'contatos' """
+app.config['MYSQL_DB'] = 'desafio3'
+
+mysql = MySQL(app)
+
+@app.route('/desafio3', methods=['GET', 'POST'])
+def desafio3():
+    if request.method == 'POST':
+        email = request.form['email']
+        assunto = request.form['assunto']
+        descricao = request.form['descricao']
+        
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO contatos(email, assunto, descricao) VALUES(%s, %s, %s)', (email, assunto, descricao))
+        
+        mysql.connection.commit()
+        
+        cur.close()
+        
+        return 'sucesso'
+    return render_template('contatos.html')   
 
 @app.route("/")
 def home():
